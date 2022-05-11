bf9a58a94b7acd756f9a1ac0a5cb1a75b4ba2240
Scifiler-sys <pagdilaostephen@gmail.com>
2022-05-11 10:02:57 -0400

    Updated notes

--- Wk3-OOP/Poke/PokeUI/Program.cs
+++ Wk3-OOP/Poke/PokeUI/Program.cs
@@ -2,35 +2,6 @@
 using PokeBL;
 using PokeUI;
 
-//=====================================
-// Useful for debugging model validation
-// Console.WriteLine("Hello, World!");
-
-// //Creating an object
-// Pokemon pokeObj = new Pokemon();
-// pokeObj.PokeID = 100;
-// Console.WriteLine(pokeObj.PokeID);
-// pokeObj.PokeID = -10;
-// Console.WriteLine(pokeObj.PokeID);
-
-// //Adding abilities to obj
-// Ability abi1 = new Ability();
-// abi1.Name = "Tackle";
-// Ability abi2 = new Ability();
-// abi2.Name = "Screech";
-
-// pokeObj.Abilities.Add(abi1);
-// pokeObj.Abilities.Add(abi2);
-
-// //Display everything that pokemon obj currently has in its abilities
-// foreach (Ability item in pokeObj.Abilities)
-// {
-//     Console.WriteLine(item.Name);
-// }
-//=======================================
-
-Console.Clear();
-
 //Creating MainMenu obj
 // Another form of polymorphism called Variance
 // Variance - that is letting a reference variable have multiple forms/objects hold by having it define as an interface 
@@ -43,7 +14,7 @@ while (repeat)
     Console.Clear();
     menu.Display();
     string ans = menu.YourChoice();
-
+    
     if (ans == "MainMenu")
     {
         menu = new MainMenu();
